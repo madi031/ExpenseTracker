540d08cfc61a6bc24f8a309f1083567d05b70dd4
mathioli <mathioli@ibm.com>
2019-03-28 00:39:03 -0400

    Add biometric id support

---
+++ Expense Tracker/Biometric/BiometricIDAuthentication.swift
@@ -0,0 +1,73 @@
+//
+//  BiometricIDAuthentication.swift
+//  Expense Tracker
+//
+//  Created by madi on 3/28/19.
+//  Copyright © 2019 com.madi.budget. All rights reserved.
+//
+
+import LocalAuthentication
+import Foundation
+
+class BiometricIDAuthentication {
+    let context = LAContext()
+    var loginReason = "Prove it is you!!"
+    
+    enum BiometricType {
+        case none
+        case faceID
+        case touchID
+    }
+    
+    func canEvaluateBiometricPolicy()-> Bool {
+        return context.canEvaluatePolicy(.deviceOwnerAuthentication, error: nil)
+    }
+    
+    func autheticateUser(completion: @escaping(String?) -> Void) {
+        guard canEvaluateBiometricPolicy() else {
+            completion("Touch/Face ID not available")
+            return
+        }
+        
+        context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: loginReason) { (success, evaluateError) in
+            if success {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+            } else {
+                let message: String
+                
+                switch evaluateError {
+                case LAError.authenticationFailed?:
+                    message = "There was a problem verifying your identity."
+                case LAError.userCancel?:
+                    message = "You pressed cancel."
+                case LAError.biometryNotAvailable?:
+                    message = "Face ID/Touch ID is not available."
+                case LAError.biometryNotEnrolled?:
+                    message = "Face ID/Touch ID is not set up."
+                case LAError.biometryLockout?:
+                    message = "Face ID/Touch ID is locked."
+                default:
+                    message = "Face ID/Touch ID may not be configured"
+                }
+                
+                completion(message)
+            }
+        }
+    }
+    
+    func biometricType() -> BiometricType {
+        let _ = canEvaluateBiometricPolicy()
+        switch context.biometryType {
+        case .none:
+            return .none
+        case .faceID:
+            return .faceID
+        case .touchID:
+            return .touchID
+        default:
+            return .none
+        }
+    }
+}
---
+++ Expense Tracker/Biometric/BiometricViewController.swift
@@ -0,0 +1,50 @@
+//
+//  BiometricViewController.swift
+//  Expense Tracker
+//
+//  Created by madi on 3/27/19.
+//  Copyright © 2019 com.madi.budget. All rights reserved.
+//
+
+import UIKit
+
+class BiometricViewController: UIViewController {
+    
+    @IBOutlet weak var authenticateButton: UIButton!
+    
+    let touchMe = BiometricIDAuthentication()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        switch touchMe.biometricType() {
+        case .faceID:
+            authenticateButton.setImage(UIImage(named: "FaceID"), for: .normal)
+        default:
+            authenticateButton.setImage(UIImage(named: "TouchID"), for: .normal)
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        loginAction()
+    }
+    
+    func loginAction() {
+        touchMe.autheticateUser { [weak self] message in
+            if let message = message {
+                let alertView = UIAlertController(title: "Oops!!", message: message, preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "Darn!", style: .default)
+                alertView.addAction(okAction)
+                self?.present(alertView, animated: true, completion: nil)
+            } else {
+                self?.performSegue(withIdentifier: "authenticateUserSegue", sender: self)
+            }
+        }
+    }
+    
+    @IBAction func authenticateButtonTapped(_ sender: Any) {
+        loginAction()
+    }
+}
